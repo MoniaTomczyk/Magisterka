1f310ecdd698a013fa01c0e3337760cc5e04940c
Monika tomczyk <monikatomczyk93@gmail.com>
2018-01-18 20:45:57 +0100

    error with  orm

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
-require File.expand_path('../boot', __FILE__)
-
+#require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -15,8 +15,9 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CustomThemeRails
+module Magisterka
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "pages#index"
+  get '/home', to: 'static_pages#home'
 end
