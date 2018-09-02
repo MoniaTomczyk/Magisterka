32552536a459f18618f120afaf68720a0b92901c
Monika tomczyk <monikatomczyk93@gmail.com>
2018-09-02 12:27:36 +0200

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,17 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'https://medicalappproject1.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "monikatomczyk93@gmail.com",
-      password: "monika1monika1"
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :domain => 'https://medicalappproject1.herokuapp.com/',
+    :user_name => 'monikatomczyk93@gmail.com',
+    :password => 'monika1monika1'
 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
