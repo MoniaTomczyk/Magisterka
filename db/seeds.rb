49f3719fe59a84a5ff7a51e54f1f24aa45a9a8b9
Monika tomczyk <monikatomczyk93@gmail.com>
2018-09-02 14:57:07 +0200

    database admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+AdminUser.create!(email: 'admin@gmail.com', password: 'password', password_confirmation: 'password')
 AdminUser.create!(email: 'admin@gmail.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 #Specialization.create!(name: 'Laryngolog')
